fee23ea64cf9c118d460dd57ed5ea6bc74f88904
Amir Samson <amirhsamson93@gmail.com>
2026-02-14 15:22:59 +0330

    Updated the test file -> App.java

--- appium-java/src/main/java/com/amir/appium/App.java
+++ appium-java/src/main/java/com/amir/appium/App.java
@@ -2,7 +2,9 @@ package com.amir.appium;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
@@ -17,7 +19,13 @@ public class App
         capabilities.setCapability("appium:platformVersion","16");       
         capabilities.setCapability("appium:appPackage","com.android.settings");
         capabilities.setCapability("appium:appActivity","com.android.settings.Settings");
-        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),capabilities);   
-        // System.out.println( "Hello World!" );
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),capabilities);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        By searhBy = By.id("com.android.settings:id/search_bar_title"); // We defined the Searching ID so that we can use it in different places.
+        driver.findElement(searhBy).click();
+        driver.findElement(By.id("com.google.android.settings.intelligence:id/open_search_view_edit_text")).sendKeys("Battery");
+        driver.findElement(By.id("android:id/switch_widget")).click();
+        driver.quit();
     }
 }
