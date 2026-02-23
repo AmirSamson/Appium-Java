281346621bd16358745f8b1fbb86317e579f4155
Amir Samson <amirhsamson93@gmail.com>
2026-02-23 19:44:18 +0330

    updted the BaseDriver.java file

--- appium-java/src/main/java/DriverManagement/BaseDriver.java
+++ appium-java/src/main/java/DriverManagement/BaseDriver.java
@@ -1,5 +1,29 @@
 package DriverManagement;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.time.Duration;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;;
 
 public class BaseDriver {
+
+    AndroidDriver driver ;
     
+    public AndroidDriver initAndroidDriver() throws MalformedURLException{
+        
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),getDesiredCapabilities());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
+    }
+
+    private static DesiredCapabilities getDesiredCapabilities(){
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("appium:automationName","UIAutomator2");
+        capabilities.setCapability("appium:platformVersion","16");       
+        capabilities.setCapability("app", System.getProperty("user.dir")+
+        "appium-java\\src\\main\\resources\\App\\amazon.mShop.android.shopping.apk");
+        capabilities.setCapability("newCommandTimeout",120);
+        return capabilities;
+    }
 }
