886622e4422d8d6fcb215621c3130c2f41557e00
Amir Samson <amirhsamson93@gmail.com>
2026-03-02 16:43:03 +0330

    Added the Appium serivce in BaseDriver file, for running the APpium server automatically

--- appium-java/src/main/java/DriverManagement/BaseDriver.java
+++ appium-java/src/main/java/DriverManagement/BaseDriver.java
@@ -3,15 +3,19 @@ import java.net.URL;
 import java.net.MalformedURLException;
 import java.time.Duration;
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 public class BaseDriver {
 
     AndroidDriver driver ;
-    
+    AppiumDriverLocalService appiumService;
+
     @SuppressWarnings("null")
     public AndroidDriver initAndroidDriver() throws MalformedURLException{
-        
+        initAppiumService();
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),getDesiredCapabilities());
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
@@ -21,12 +25,12 @@ public class BaseDriver {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName","Android");
         capabilities.setCapability("appium:automationName","UIAutomator2");
-        capabilities.setCapability("appium:platformVersion","16");       
+        capabilities.setCapability("appium:platformVersion","16");     
+          
         capabilities.setCapability(
-    "appium:app",
-    System.getProperty("user.dir") +
-    "\\src\\main\\resources\\App\\amazon.mShop.android.shopping.apk"   //appium-java\src\main\resources\App\amazon.mShop.android.shopping.apk
-);
+            "appium:app",
+            System.getProperty("user.dir") +
+            "\\src\\main\\resources\\App\\amazon.mShop.android.shopping.apk"); //appium-java\src\main\resources\App\amazon.mShop.android.shopping.apk
 
         capabilities.setCapability("appium:newCommandTimeout",120);
         return capabilities;
