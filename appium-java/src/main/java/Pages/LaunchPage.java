fe13c483338ad0106973977e4bdd9764f2e41a7f
Amir Samson <amirhsamson93@gmail.com>
2026-03-17 14:14:10 +0330

    added the WebElement usage for finding ids on pages

--- appium-java/src/main/java/Pages/LaunchPage.java
+++ appium-java/src/main/java/Pages/LaunchPage.java
@@ -1,11 +1,7 @@
 package Pages;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
-
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.pagefactory.AndroidBy;
+
 
 public class LaunchPage extends BasePage {
 
@@ -14,7 +10,7 @@ public class LaunchPage extends BasePage {
         // PageFactory.initElements(driver, this);  this is for "WebElement"
     }
 
-    
+
     /**
      * we can also use the WebElement to find the elements and use them.
      * However there is a downside to it. if the DOM changes, all tests will fail because it cannot find the DOM and ID.
